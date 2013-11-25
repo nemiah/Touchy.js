202128adea71f3ae257a7ab4f26824acd597340c
Rainer Furtmeier <Rainer@Furtmeier.IT>
2013-11-25 17:14:07 +0100

    improved wheel

--- Touchy.js
+++ Touchy.js
@@ -21,18 +21,25 @@
 var Touchy = {
 	jQuery: $,
 			
-	wheel: function(element, options){
-		Touchy.jQuery(element).click(function(event){
-			Touchy.wheelOnFire(event, options)
+	wheel: function(element, options, triggerOn){
+		if(typeof triggerOn === "undefined")
+			triggerOn = "click";
+		
+		Touchy.jQuery(element).on(triggerOn, function(event){
+			Touchy.wheelOnFire(event, options);
 		});
 	},
 			
 	wheelOnFire: function(event, options){
 		var circles = options.data;
-		
-		var winkel = Math.PI * 1.9 / circles.length;
-		var radiusBig = 70;
-		var radiusSmall = (2 * Math.PI * radiusBig) / circles.length / 2.3;
+
+		var length = 0;
+		for(var propertyName in circles)
+			length++;
+
+		var winkel = Math.PI * 1.9 / length;
+		var radiusBig = 100;
+		var radiusSmall = (2 * Math.PI * radiusBig) / length / 2.3;
 		
 		var width = radiusBig * 2 + radiusSmall * 2 + 10;
 		var height = width;
@@ -58,33 +65,56 @@ var Touchy = {
 		bigC.attr({"fill-opacity": 0, "class": "touchy-wheel-background"});
 		bigC.animate({"fill-opacity": .5}, 700);
 		
-		for(var i = 0; i < circles.length; i++){
+		var i = 0;
+		for(var value in circles){
 			var x = Math.cos(i * winkel - (Math.PI / 4)) * radiusBig;
 			var y = Math.sin(i * winkel - (Math.PI / 4)) * radiusBig;
 
 			var c = s.circle(startX + x, startY + y, 0);
 			c.attr({"class": "touchy-wheel-circle", "fill-opacity": 1});
 
-			var tt = s.text(-100, -100, circles[i]);
-			var t = s.text(startX + x - (tt.getBBox().width / 2), startY + y + (tt.getBBox().height / 4), circles[i]);
+			var tt = s.text(-100, -100, circles[value]);
+			var t = s.text(startX + x - (tt.getBBox().width / 2), startY + y + (tt.getBBox().height / 4), circles[value]);
 			t.attr({ "fill-opacity": 0, "class": "touchy-wheel-text" });
 			tt.remove();
 
 
 			g = s.g(c, t);
+			g.node.touchyValue = value;
+			
+			
 			g.mouseover(function(){
 				this.select("circle").animate({r: radiusSmall + 5}, 100);
 			});
+			
+			g.touchstart(function(){
+				this.select("circle").animate({r: radiusSmall + 5}, 100);
+			});
+
 
 			g.mouseout(function(){
 				this.select("circle").animate({r: radiusSmall}, 100);
 			});
 
-			g.click(function(){
+			g.touchend(function(){
+				this.select("circle").animate({r: radiusSmall}, 100);
+			});
+			
+
+			g.touchcancel(function(){
+				this.select("circle").animate({r: radiusSmall}, 100);
+			});
+
+			Touchy.jQuery(g.node).on("click touchend", function(event){
+				event.preventDefault();
+				event.stopPropagation();
+				//console.log(Touchy.jQuery(event.target).parent().get(0).touchyValue);
+				options.selection(Touchy.jQuery(event.target).parent().get(0).touchyValue);
+				
 				var current = this;
 				var i = 0;
-				Touchy.jQuery(this.parent().node).find("g").each(function(v, k){
-					if(current.node.isEqualNode(k))
+				Touchy.jQuery(this).parent().find("g").each(function(v, k){
+					if(current.isEqualNode(k))
 						return true;
 					
 					Snap(k).select("text").animate({"fill-opacity": 0}, 100, null, function(){ this.remove(); });
@@ -92,27 +122,30 @@ var Touchy = {
 					i++;
 				});
 				
-				current.parent().select("circle").animate({"fill-opacity": 0}, 100);
+				Snap(current).parent().select("circle").animate({"fill-opacity": 0}, 100);
 				
 				window.setTimeout(function(){
-					current.select("circle").animate({r: 0}, 200, null, function(){
+					Snap(current).select("circle").animate({r: 0}, 200, null, function(){
 						this.parent().parent().remove();
 						this.remove();
 					});
-					current.select("text").animate({"fill-opacity": 0}, 100, null, function(){ });
+					Snap(current).select("text").animate({"fill-opacity": 0}, 100, null, function(){ });
 				}, 500);
+				
+				return false;
 			});
 
 			window.setTimeout(function(lc){
-				lc.animate({r: radiusSmall}, 300);
-			}, i * 100, c);
+				lc.animate({r: radiusSmall}, 150);
+			}, i * 80, c);
 
 			window.setTimeout(function(lt){
 				lt.animate({
 					"fill-opacity": 1
-				}, 300);
-			}, i * 100 + 300, t);
+				}, 200);
+			}, i * 100 + 200, t);
 
+			i++;
 		}
 	}
 };
