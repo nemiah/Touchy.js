9ba878991b1a4716c592fd91809aceed791c561b
Rainer Furtmeier <Rainer@Furtmeier.IT>
2013-11-25 02:00:28 +0100

    added wheel

---
+++ Touchy.js
@@ -0,0 +1,118 @@
+/*
+ *
+ *  This file is part of Touchy.js.
+
+ *  Touchy.js is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  Touchy.js is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *  2013, Rainer Furtmeier - Rainer@Furtmeier.IT
+ */
+
+var Touchy = {
+	jQuery: $,
+			
+	wheel: function(element, options){
+		Touchy.jQuery(element).click(function(event){
+			Touchy.wheelOnFire(event, options)
+		});
+	},
+			
+	wheelOnFire: function(event, options){
+		var circles = options.data;
+		
+		var winkel = Math.PI * 1.9 / circles.length;
+		var radiusBig = 70;
+		var radiusSmall = (2 * Math.PI * radiusBig) / circles.length / 2.3;
+		
+		var width = radiusBig * 2 + radiusSmall * 2 + 10;
+		var height = width;
+		
+		var s = Snap(width, height);
+		Touchy.jQuery(s.node).css("position","absolute");
+		
+		var posX = event.clientX - width / 2;
+		var posY = event.clientY - height / 2;
+		
+		if(posX < 10)
+			posX = 10;
+		if(posY < 10)
+			posY = 10;
+		
+		Touchy.jQuery(s.node).css("position","absolute").css("top", posY).css("left", posX);
+		
+		
+		var startX = radiusBig + radiusSmall + 5;
+		var startY = radiusBig + radiusSmall + 5;
+		
+		var bigC = s.circle(startX, startY, radiusBig);
+		bigC.attr({"fill-opacity": 0, "class": "touchy-wheel-background"});
+		bigC.animate({"fill-opacity": .5}, 700);
+		
+		for(var i = 0; i < circles.length; i++){
+			var x = Math.cos(i * winkel - (Math.PI / 4)) * radiusBig;
+			var y = Math.sin(i * winkel - (Math.PI / 4)) * radiusBig;
+
+			var c = s.circle(startX + x, startY + y, 0);
+			c.attr({"class": "touchy-wheel-circle", "fill-opacity": 1});
+
+			var tt = s.text(-100, -100, circles[i]);
+			var t = s.text(startX + x - (tt.getBBox().width / 2), startY + y + (tt.getBBox().height / 4), circles[i]);
+			t.attr({ "fill-opacity": 0, "class": "touchy-wheel-text" });
+			tt.remove();
+
+
+			g = s.g(c, t);
+			g.mouseover(function(){
+				this.select("circle").animate({r: radiusSmall + 5}, 100);
+			});
+
+			g.mouseout(function(){
+				this.select("circle").animate({r: radiusSmall}, 100);
+			});
+
+			g.click(function(){
+				var current = this;
+				var i = 0;
+				Touchy.jQuery(this.parent().node).find("g").each(function(v, k){
+					if(current.node.isEqualNode(k))
+						return true;
+					
+					Snap(k).select("text").animate({"fill-opacity": 0}, 100, null, function(){ this.remove(); });
+					Snap(k).select("circle").animate({r: 0}, 100 + i * 100, null, function(){ this.remove(); });
+					i++;
+				});
+				
+				current.parent().select("circle").animate({"fill-opacity": 0}, 100);
+				
+				window.setTimeout(function(){
+					current.select("circle").animate({r: 0}, 200, null, function(){
+						this.parent().parent().remove();
+						this.remove();
+					});
+					current.select("text").animate({"fill-opacity": 0}, 100, null, function(){ });
+				}, 500);
+			});
+
+			window.setTimeout(function(lc){
+				lc.animate({r: radiusSmall}, 300);
+			}, i * 100, c);
+
+			window.setTimeout(function(lt){
+				lt.animate({
+					"fill-opacity": 1
+				}, 300);
+			}, i * 100 + 300, t);
+
+		}
+	}
+};
