fe36a304c39190c726b85311b77dc512804718e0
Rainer Furtmeier <Rainer@Furtmeier.IT>
2013-11-29 17:08:03 +0100

    refactoring

--- Touchy.js
+++ Touchy.js
@@ -73,102 +73,24 @@ var Touchy = {
 		var startX = radiusBig + radiusSmall + 5;
 		var startY = radiusBig + radiusSmall + 5;
 		
-		var bigC = s.circle(startX, startY, radiusBig + radiusSmall + 5);
-		bigC.attr({"fill-opacity": 0, "class": "touchy-wheel-background"});
-		bigC.animate({"fill-opacity": .5}, 700);
-		
 		var currentValue = null;
 		if(typeof options.value === "function")
 			currentValue = options.value();
 		
-		var i = 0;
-		for(var value in circles){
-			var x = Math.cos(i * winkel - (Math.PI / 4)) * radiusBig;
-			var y = Math.sin(i * winkel - (Math.PI / 4)) * radiusBig;
-
-			var c = s.circle(startX + x, startY + y, 0);
-			c.attr({"class": "touchy-wheel-circle"+(currentValue == value ? " touchy-wheel-value" : ""), "fill-opacity": 1});
-
-			var tt = s.text(-100, -100, circles[value]);
-			var t = s.text(startX + x - (tt.getBBox().width / 2), startY + y + (tt.getBBox().height / 4), circles[value]);
-			t.attr({ "fill-opacity": 0, "class": "touchy-wheel-text" });
-			tt.remove();
-
-
-			g = s.g(c, t);
-			g.node.touchyValue = value;
-			
-			
-			g.mouseover(function(){
-				this.select("circle").animate({r: radiusSmall + 5}, 100);
-			});
-			
-			g.touchstart(function(){
-				this.select("circle").animate({r: radiusSmall + 5}, 100);
-			});
-
-
-			g.mouseout(function(){
-				this.select("circle").animate({r: radiusSmall}, 100);
-			});
-
-			g.touchend(function(){
-				this.select("circle").animate({r: radiusSmall}, 100);
-			});
-			
-
-			g.touchcancel(function(){
-				this.select("circle").animate({r: radiusSmall}, 100);
-			});
-
-			window.setTimeout(function(lc){
-				lc.animate({r: radiusSmall}, 150);
-			}, i * 80, c);
-
-			window.setTimeout(function(lt){
-				lt.animate({
-					"fill-opacity": 1
-				}, 200);
-			}, i * 100 + 200, t);
-
-			i++;
-		}
+		var i = Touchy.drawWheel(s, [startX, startY], circles, radiusBig, radiusSmall, winkel, currentValue);
 		
 		
 		var x = Math.cos(i * winkel - (Math.PI / 4)) * radiusBig;
 		var y = Math.sin(i * winkel - (Math.PI / 4)) * radiusBig;
 
-		var c = s.circle(startX + x, startY + y, 0);
-		c.attr({"class": "touchy-wheel-circle", "fill-opacity": 1});
 
-		g = s.g(c);
-		g.attr({"class": "touchy-wheel-close", "fill-opacity": 0});
-			
-		g.mouseover(function(){
-			this.select("circle").animate({r: radiusSmall + 5}, 100);
-		});
-
-		g.touchstart(function(){
-			this.select("circle").animate({r: radiusSmall + 5}, 100);
-		});
-
-
-		g.mouseout(function(){
-			this.select("circle").animate({r: radiusSmall}, 100);
-		});
-
-		g.touchend(function(){
-			this.select("circle").animate({r: radiusSmall}, 100);
-		});
-
-
-		g.touchcancel(function(){
-			this.select("circle").animate({r: radiusSmall}, 100);
-		});
+		var g = Touchy.drawCancel(s, [startX + x, startY + y]);
+		Touchy.animateCircle(g, radiusSmall, radiusSmall + 5);
 		
 		window.setTimeout(function(lc){
 			lc.animate({r: radiusSmall}, 150);
-		}, i * 80, c);
+		}, i * 80, g.select("circle"));
+		
 		
 		Touchy.jQuery(s.node).find("g").on(Touchy.trigger, function(event){
 			event.preventDefault();
@@ -210,6 +132,98 @@ var Touchy = {
 			}, 500);
 
 			return false;
+		});
+	},
+	
+	drawWheel: function(s, center, values, radiusBig, radiusSmall, winkel, currentValue){
+		var startX = center[0];
+		var startY = center[1];
+		
+		
+		var bigC = s.circle(startX, startY, radiusBig + radiusSmall + 5);
+		bigC.attr({"fill-opacity": 0, "class": "touchy-wheel-background"});
+		bigC.animate({"fill-opacity": .5}, 700);
+		
+		
+		var i = 0;
+		for(var key in values){
+			var x = Math.cos(i * winkel - (Math.PI / 4)) * radiusBig;
+			var y = Math.sin(i * winkel - (Math.PI / 4)) * radiusBig;
+
+
+			var g = Touchy.drawCircle(s, [startX + x, startY + y], key, values[key], currentValue == key);
+			
+			Touchy.animateCircle(g, radiusSmall, radiusSmall + 5);
+
+
+			window.setTimeout(function(lc){
+				lc.animate({r: radiusSmall}, 150);
+			}, i * 80, g.select("circle"));
+
+			window.setTimeout(function(lt){
+				lt.animate({
+					"fill-opacity": 1
+				}, 200);
+			}, i * 100 + 200, g.select("text"));
+
+			i++;
+		}
+		
+		return i;
+	},
+	
+	drawCancel: function(s, center){
+		var x = center[0];
+		var y = center[1];
+		
+		var c = s.circle(x, y, 0);
+		c.attr({"class": "touchy-wheel-circle", "fill-opacity": 1});
+
+		g = s.g(c);
+		g.attr({"class": "touchy-wheel-cancel", "fill-opacity": 0});
+		
+		return g;
+	},
+			
+	drawCircle: function(s, center, value, label, isSelected){
+		var x = center[0];
+		var y = center[1];
+		
+		var c = s.circle(x, y, 0);
+		c.attr({"class": "touchy-wheel-circle"+(isSelected ? " touchy-wheel-value" : ""), "fill-opacity": 1});
+
+		var tt = s.text(-100, -100, label);
+		var t = s.text(x - (tt.getBBox().width / 2), y + (tt.getBBox().height / 4), label);
+		t.attr({ "fill-opacity": 0, "class": "touchy-wheel-text" });
+		tt.remove();
+
+		var g = s.g(c, t);
+		g.node.touchyValue = value;
+		
+		return g;
+	},
+			
+	animateCircle: function(g, radiusDefault, radiusOver){
+		g.mouseover(function(){
+			this.select("circle").animate({r: radiusOver}, 100);
+		});
+
+		g.touchstart(function(){
+			this.select("circle").animate({r: radiusOver}, 100);
+		});
+
+
+		g.mouseout(function(){
+			this.select("circle").animate({r: radiusDefault}, 100);
+		});
+
+		g.touchend(function(){
+			this.select("circle").animate({r: radiusDefault}, 100);
+		});
+
+
+		g.touchcancel(function(){
+			this.select("circle").animate({r: radiusDefault}, 100);
 		});
 	}
 };
